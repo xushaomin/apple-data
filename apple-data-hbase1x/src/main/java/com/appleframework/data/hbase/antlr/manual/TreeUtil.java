253dc4938bd3dd81452a1dfe98b0d6132d48b063
Cruise.Xu <xushaomin@foxmail.com>
2019-01-10 15:36:52 +0800

    新增hbase2

--- apple-data-hbase1x/src/main/java/com/appleframework/data/hbase/antlr/manual/TreeUtil.java
+++ apple-data-hbase1x/src/main/java/com/appleframework/data/hbase/antlr/manual/TreeUtil.java
@@ -25,17 +25,15 @@ public class TreeUtil {
      * */
     public static ProgContext parseProgContext(String hql) {
         Util.checkEmptyString(hql);
-
-        try {
-            ANTLRInputStream input = new ANTLRInputStream(new StringReader(hql));
-            SimpleHbaseStatementsLexer lexer = new SimpleHbaseStatementsLexer(
-                    input);
-            CommonTokenStream tokens = new CommonTokenStream(lexer);
-            StatementsParser parser = new StatementsParser(tokens);
-            parser.setErrorHandler(SimpleHbaseErrorStrategy.instance);
-            ProgContext progContext = parser.prog();
-            return progContext;
-        } catch (Exception e) {
+		try {
+			ANTLRInputStream input = new ANTLRInputStream(new StringReader(hql));
+			SimpleHbaseStatementsLexer lexer = new SimpleHbaseStatementsLexer(input);
+			CommonTokenStream tokens = new CommonTokenStream(lexer);
+			StatementsParser parser = new StatementsParser(tokens);
+			parser.setErrorHandler(SimpleHbaseErrorStrategy.instance);
+			ProgContext progContext = parser.prog();
+			return progContext;
+		} catch (Exception e) {
             throw new SimpleHBaseException("parse error.", e);
         }
     }
@@ -45,22 +43,15 @@ public class TreeUtil {
      * */
     public static String parseTableName(ProgContext progContext) {
         Util.checkNull(progContext);
-
         if (progContext instanceof InsertHqlClContext) {
-            return InsertHqlClContext.class.cast(progContext).inserthqlc()
-                    .tablename().TEXT().getText();
+            return InsertHqlClContext.class.cast(progContext).inserthqlc().tablename().TEXT().getText();
         }
-
         if (progContext instanceof SelectHqlClContext) {
-            return SelectHqlClContext.class.cast(progContext).selecthqlc()
-                    .tablename().TEXT().getText();
+            return SelectHqlClContext.class.cast(progContext).selecthqlc().tablename().TEXT().getText();
         }
-
         if (progContext instanceof DeleteHqlClContext) {
-            return DeleteHqlClContext.class.cast(progContext).deletehqlc()
-                    .tablename().TEXT().getText();
+            return DeleteHqlClContext.class.cast(progContext).deletehqlc().tablename().TEXT().getText();
         }
-
         throw new SimpleHBaseException("parse error.");
     }
 
@@ -69,19 +60,15 @@ public class TreeUtil {
      * */
     public static RawHQLType parseHQLType(ProgContext progContext) {
         Util.checkNull(progContext);
-
         if (progContext instanceof InsertHqlClContext) {
             return RawHQLType.PUT;
         }
-
         if (progContext instanceof SelectHqlClContext) {
             return RawHQLType.SELECT;
         }
-
         if (progContext instanceof DeleteHqlClContext) {
             return RawHQLType.DELETE;
         }
-
         throw new SimpleHBaseException("parse error.");
     }
 
